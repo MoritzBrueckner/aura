60f4dbdbd50adde0fbdd4859eee109c5d1d0f30f
Moritz Brückner <mail@moritz-brueckner.de>
2021-08-27 00:36:33 +0200

    Improve assert() macro and add optional message param

--- Sources/aura/utils/Assert.hx
+++ Sources/aura/utils/Assert.hx
@@ -1,25 +1,33 @@
 package aura.utils;
 
-import haxe.macro.Context;
-import haxe.macro.PositionTools;
+import haxe.Exception;
 import haxe.PosInfos;
 import haxe.exceptions.PosException;
+import haxe.macro.Context;
 import haxe.macro.Expr;
 
 using haxe.macro.ExprTools;
 
 class Assert {
 	/**
-		Every assert statement with a level below this threshold is ignored.
-		This variable can be set via the compiler flag `AURA_ASSERT_LEVEL`.
-	**/
-	static var assertThreshold = AssertLevel.Critical;
+		Checks whether the given expression evaluates to true. If this is not
+		the case, a `AuraAssertionException` with additional information is
+		thrown.
 
-	public static macro function assert(levelExpr: ExprOf<AssertLevel>, condition: ExprOf<Bool>): Expr {
-		final level: AssertLevel = AssertLevel.fromExpr(levelExpr);
+		The assert level describes the severity of the assertion. If the
+		severity is lower than the level stored in the `AURA_ASSERT_LEVEL` flag,
+		the assertion is omitted from the code so that it doesn't decrease the
+		runtime performance.
+
+		@param level The severity of this assertion.
+		@param condition The conditional expression to test.
+		@param message Optional message to display when the assertion fails.
+	**/
+	public static macro function assert(level: ExprOf<AssertLevel>, condition: ExprOf<Bool>, message: String = ""): Expr {
+		final levelVal: AssertLevel = AssertLevel.fromExpr(level);
 		final assertThreshold = AssertLevel.fromString(Context.definedValue("AURA_ASSERT_LEVEL"));
 
-		if (level < assertThreshold) {
+		if (levelVal < assertThreshold) {
 			return macro {};
 		}
 
@@ -28,7 +36,7 @@ class Assert {
 				#if AURA_ASSERT_QUIT kha.System.stop(); #end
 
 				@:pos(condition.pos)
-				@:privateAccess throwAssertionError($v{condition.toString()});
+				@:privateAccess throwAssertionError($v{condition.toString()}, $v{message});
 			}
 		};
 	}
@@ -37,36 +45,60 @@ class Assert {
 		Helper function to prevent Haxe "bug" that actually throws an error
 		even when using `macro throw` (inlining this method also does not work).
 	**/
-	static function throwAssertionError(exprString: String, ?pos: PosInfos) {
-		throw new PosException('\n[Aura] Failed assertion: \n|\t$exprString\n|  ', pos);
+	static function throwAssertionError(exprString: String, message: String, ?pos: PosInfos) {
+		throw new AuraAssertionException(exprString, message, pos);
+	}
+}
+
+/**
+	Exception that is thrown when an assertion fails.
+
+	@see `Assert`
+**/
+class AuraAssertionException extends PosException {
+
+	/**
+		@param exprString The string representation of the failed assert condition.
+		@param message Custom error message, use an empty string to omit this.
+	**/
+	public function new(exprString: String, message: String, ?previous: Exception, ?pos: Null<PosInfos>) {
+		final optMsg = message != "" ? '\n\tMessage: $message' : "";
+
+		super('\n[Aura] Failed assertion:$optMsg\n\tExpression: ($exprString)', previous, pos);
 	}
 }
 
 enum abstract AssertLevel(Int) from Int to Int {
-	var Hidden: AssertLevel = 0;
-	var Debug: AssertLevel = 1;
-	var Warning: AssertLevel = 2;
-	var Error: AssertLevel = 3;
-	var Critical: AssertLevel = 4;
+	var Debug: AssertLevel;
+	var Warning: AssertLevel;
+	var Error: AssertLevel;
+	var Critical: AssertLevel;
 
-	@:op(A < B) static function lt(a:AssertLevel, b:AssertLevel):Bool;
-	@:op(A > B) static function gt(a:AssertLevel, b:AssertLevel):Bool;
+	// Don't use this level in assert() calls!
+	var NoAssertions: AssertLevel;
 
 	public static function fromExpr(e: ExprOf<AssertLevel>): AssertLevel {
 		switch (e.expr) {
 			case EConst(CIdent(v)): return fromString(v);
-			default: throw 'Unsupported expression: $e';
+			default: throw new Exception('Unsupported expression: $e');
 		};
 	}
 
+	/**
+		Converts a string into an `AssertLevel`, the string must be spelled
+		exactly as the assert level. `null` defaults to `AssertLevel.Critical`.
+	**/
 	public static function fromString(s: String): AssertLevel {
 		return switch (s) {
-			case "Hidden": Hidden;
 			case "Debug": Debug;
 			case "Warning": Warning;
 			case "Error": Error;
 			case "Critical" | null: Critical;
-			default: throw 'Could not convert string to AssertLevel: $s';
+			case "NoAssertions": NoAssertions;
+			default: throw 'Could not convert "$s" to AssertLevel';
 		}
 	}
+
+	@:op(A < B) static function lt(a:AssertLevel, b:AssertLevel):Bool;
+	@:op(A > B) static function gt(a:AssertLevel, b:AssertLevel):Bool;
 }
