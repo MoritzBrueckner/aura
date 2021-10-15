048e8926f3f104ce119fb1dca753ddbbdfc85669
Moritz Brückner <mail@moritz-brueckner.de>
2021-10-15 17:22:02 +0200

    Update hxdsp to c4a7001bc5c8d0f9e6d3cd583acf37a3f71d8fd4

--- khafile.js
+++ khafile.js
@@ -7,19 +7,8 @@ let project = new Project('aura');
 
 project.addSources('Sources');
 
-// Kha can't include libraries where the sources folder
-// isn't called "Sources", so manually include the sources
-// here. Also, the hxdsp library includes a Main.hx as an
-// example that we need to ignore because it requires another
-// library and it would override the project's Main.hx.
-callbacks.preHaxeCompilation = () => {
-	const hxdspSrc = libPath + "/hxdsp/src/";
-	fs.renameSync(hxdspSrc + "Main.hx", hxdspSrc + "Main.temp.txt");
-};
-callbacks.postHaxeCompilation = () => {
-	const hxdspSrc = libPath + "/hxdsp/src/";
-	fs.renameSync(hxdspSrc + "Main.temp.txt", hxdspSrc + "Main.hx");
-};
+// Kha can't include libraries where the sources folder isn't
+// called "Sources", so manually include the sources here
 project.addSources("Libraries/hxdsp/src");
 
 resolve(project);
