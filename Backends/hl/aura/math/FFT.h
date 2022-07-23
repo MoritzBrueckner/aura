aa4900b9c0b90feb16327cf89e1ad9ece607a1de
Moritz Brückner <17685000+MoritzBrueckner@users.noreply.github.com>
2022-07-24 01:57:24 +0200

    Fix compilation of the HL backend

--- Backends/hl/aura/math/FFT.h
+++ Backends/hl/aura/math/FFT.h
@@ -2,20 +2,20 @@
 
 #include <hl.h>
 
-#include <aura/types/ComplexArrayImpl.h>
+#include <aura/types/_ComplexArray/HL_ComplexArrayImpl.h>
 
 #include "hl/aura/aurahl.h"
 #include "common_c/math/fft.h"
 #include "common_c/types/complex_t.h"
 
-HL_PRIM void AURA_HL_FUNC(ditfft2)(aura__types__ComplexArrayImpl time_array, int t, aura__types__ComplexArrayImpl freq_array, int f, int n, int step, bool inverse) {
+HL_PRIM void AURA_HL_FUNC(ditfft2)(aura__types___ComplexArray__HL_ComplexArrayImpl time_array, int t, aura__types___ComplexArray__HL_ComplexArrayImpl freq_array, int f, int n, int step, bool inverse) {
 	const aura_complex_t *times = (aura_complex_t*) time_array->self;
 	aura_complex_t *freqs = (aura_complex_t*) freq_array->self;
 
 	aura_ditfft2(times, t, freqs, f, n, step, inverse);
 }
 
-HL_PRIM void AURA_HL_FUNC(ditfft2_iterative)(aura__types__ComplexArrayImpl time_array, aura__types__ComplexArrayImpl freq_array, int n, bool inverse) {
+HL_PRIM void AURA_HL_FUNC(ditfft2_iterative)(aura__types___ComplexArray__HL_ComplexArrayImpl time_array, aura__types___ComplexArray__HL_ComplexArrayImpl freq_array, int n, bool inverse) {
 	const aura_complex_t *times = (aura_complex_t*) time_array->self;
 	aura_complex_t *freqs = (aura_complex_t*) freq_array->self;
 
