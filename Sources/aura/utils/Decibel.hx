b7ecbf5607f3b9744419f3c19dcf698dc940deea
tong <tong@disktree.net>
2022-11-18 00:47:28 +0100

    Add log10 method

--- Sources/aura/utils/Decibel.hx
+++ Sources/aura/utils/Decibel.hx
@@ -1,8 +1,8 @@
 package aura.utils;
 
 /**
-    The decibel (dB) is a logarithmic unit used to express the ratio of two values of a physical quantity.
-    One of these values is often a standard reference value, in which case the decibel is used to express the level[a] of the other value relative to this reference.
+    The decibel (dB) is a relative unit of measurement equal to one tenth of a bel (B).  
+    It expresses the ratio of two values of a power or root-power quantity on a logarithmic scale.
 
     The number of decibels is ten times the logarithm to base 10 of the ratio of two power quantities.
 
@@ -14,11 +14,11 @@ package aura.utils;
 class Decibel {
 
 	public static inline function toDecibel( volume : Float ) : Float {
-		return 20 * Math.log10( volume );
+		return 20 * MathUtils.log10(volume);
 	}
 
 	public static inline function toLinear( db : Float ) : Float {
-		return Math.pow( 10, db / 20 );
+		return Math.pow(10, db / 20);
 	}
 
 }
