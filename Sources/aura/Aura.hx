e0367b437b400991897dfadd6f3aa8534fbb062f
Moisés Peláez <moises.jpelaez@gmail.com>
2025-07-08 17:25:45 -0400

    Quick count fix and small cleanup

--- Sources/aura/Aura.hx
+++ Sources/aura/Aura.hx
@@ -129,9 +129,8 @@ class Aura {
 					}
 				#end
 
-				count++;
-
 				function onChannelCountInitialized() {
+					count++;
 					if (onProgress != null) {
 						onProgress(count, length, soundName);
 					}
@@ -143,17 +142,17 @@ class Aura {
 
 				#if (kha_html5 || kha_debug_html5)
 					if (kha.SystemImpl.mobile) {
+				#end
 						// Mobile web audio channels are always decoded and
 						// the channel count is set by Kha afterwards
 						onChannelCountInitialized();
+				#if (kha_html5 || kha_debug_html5)
 					}
 					else {
 						// HACK: Kha does not set sound.channel for compressed
 						// sounds on non-mobile html5 targets, so do it manually
 						aura.channels.Html5StreamChannel.initializeChannelCount(sound, onChannelCountInitialized);
 					}
-				#else
-					onChannelCountInitialized();
 				#end
 			}, (error: kha.AssetError) -> { onLoadingError(error, failed, soundName); });
 		}
